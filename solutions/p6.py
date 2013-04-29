2f881a166fbfabb7ada3b3187b7dba32ffb0076a
Henrique Grolli Bassotto <henrique.grolli@gmail.com>
2013-04-28 21:49:59 -0300

    Adding first solution

---
+++ solutions/p6.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+size = 100
+
+sum_of_squares = sum([x*x for x in range(0, size+1)])
+square_of_sum = sum(range(0, size+1))
+square_of_sum *= square_of_sum
+
+print abs(sum_of_squares - square_of_sum)
